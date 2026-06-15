7c8ff8df3696e017480877644148deefa5d29b83
mohdadamz <mohamedelharrachi@gmail.com>
2026-06-15 14:03:37 +0400

    brand customization dark theme and fonts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WKND Harvest';
+export const SITE_DESCRIPTION = 'Your weekly harvest of tech, science, sports, culture, money and travel from around the world.';
